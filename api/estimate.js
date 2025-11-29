2a937c7028e1657895f6743f5ef19b17ab54c303
trisha-ahmed2 <trisha_ahmed@berkeley.edu>
2025-11-28 22:18:06 -0800

    Update estimate.js

--- api/estimate.js
+++ api/estimate.js
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "nodejs"
+};
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
